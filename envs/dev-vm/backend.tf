0294ea8ed3dd0ed9d972a1d08abc11d43f444c2c
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-01 20:04:21 +0100

    Update backend.tf

--- envs/dev-vm/backend.tf
+++ envs/dev-vm/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-backend"
-    storage_account_name = "tfbackendstore"
+    storage_account_name = "tfbackendstore306"
     container_name       = "tfstate"
     key                  = "vm-dev.terraform.tfstate"
   }
 }
+
