06cfa149d310289f7555282f79a6effbdbdc1ed9
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-01 15:35:48 +0100

    Update main.tf

---
+++ envs/dev-vm/main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+module "vm" {
+  source              = "../../modules/virtual_machine"
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+  vm_size             = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  environment         = var.environment
+  team                = var.team
+}
