e84d0513ddeacf4ed478e4d52417aaaaefd98415
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-04 21:51:36 +0100

    Update main.tf

--- envs/dev-vm/main.tf
+++ envs/dev-vm/main.tf
@@ -7,11 +7,11 @@ module "vm" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
-  subnet_id           = var.subnet_id
   vm_size             = var.vm_size
   admin_username      = var.admin_username
   admin_password      = var.admin_password
   environment         = var.environment
   team                = var.team
 }
+
 
